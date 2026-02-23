ab76ac10d9feeedff123e5e1a8db87c4f70999d1
aleksei.a.voitenko <aleksei.a.voitenko@jettycloud.com>
2026-02-24 00:59:09 +0200

    config: Enable React compiler and remove allowedDevOrigins from Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
+const nextConfig: NextConfig = {
   /* config options here */
-//  reactCompiler: true,
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: [
-    "http://192.168.0.103:3000",
-    "http://localhost:3000"
-  ]
+  reactCompiler: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   allowedDevOrigins: [
+//     "http://192.168.0.103:3000",
+//     "http://localhost:3000"
+//   ]
+// };
+
+// module.exports = nextConfig;
