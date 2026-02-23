ae796185a914985e094eaf09f45109229e9cdda4
alex <alex@localhost.localdomain>
2026-02-23 23:54:18 +0200

    allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+//  reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: [
+    "http://192.168.0.103:3000",
+    "http://localhost:3000"
+  ]
 };
 
-export default nextConfig;
+module.exports = nextConfig;
